9bb36a5916e6cd123c3545f553c955a2e1f245b3
Roemer Bakker <contact@roemerbakker.com>
2016-12-25 16:48:09 +0100

    Added (un)installation methods

--- db/init.php
+++ db/init.php
@@ -1,5 +1,25 @@
 <?php
 
 $queries = [
-
+    sprintf('
+        CREATE TABLE IF NOT EXISTS `%s` (
+          `transaction_id` VARCHAR(255) NOT NULL PRIMARY KEY,
+          `payment_method` VARCHAR(255) NOT NULL,
+          `cart_id` INT(64),
+          `order_id` INT(64),
+          `status` VARCHAR(255) NOT NULL,
+          `created_at` DATETIME NOT NULL,
+          `updated_at` DATETIME NOT NULL
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8;',
+        __DB_PREFIX_.'spryng_payments'
+    )
 ];
+
+foreach($queries as $query)
+{
+    if (!Db::getInstance()->execute($query))
+    {
+        $this->_errors[] = sprintf('Could not initialize database. Query: %s.', $query);
+        return false;
+    }
+}
--- spryngpayments.php
+++ spryngpayments.php
@@ -9,6 +9,10 @@ if (!defined('_PS_VERSION_'))
 
 class SpryngPayments extends PaymentModule
 {
+
+    const VERSION = '1.0';
+    const CONFIG_KEY_PREFIX = 'SPRYNG_';
+
     public $api = null;
     public $name = 'spryngpayments';
     public $tab = 'payments_gateways';
@@ -38,34 +42,51 @@ class SpryngPayments extends PaymentModule
         return Configuration::get($setting);
     }
 
-    public function uninstall()
-    {
-        if (!parent::uninstall())
-        {
-            return false;
-        }
-
-        foreach ($this->settings as $setting)
-        {
-            if (!Configuration::deleteByName($setting))
-            {
-                return false;
-            }
-        }
-
-        $this->dropDatabaseTables();
-
-        return true;
-    }
-
+    /**
+     * Installs the plugin by creating database table for payments, registering hooks and initializing default configuration.
+     *
+     * @return bool
+     */
     public function install()
     {
         if (!parent::install() || !$this->_registerHooks())
         {
-
+            $this->_errors[] = 'An error has occurred during the installation process of Spryng Payments for Prestashop.';
+            return false;
+        }
+        else if (!$this->initConfig())
+        {
+            $this->_errors[] = 'The installer was unable to initialize the default settings.';
+            return false;
         }
 
         $this->createDatabaseTables();
+
+        return true;
+    }
+
+    /**
+     * Uninstalls the plugin by dropping database table, unregistering hooks and deleting default configuration.
+     *
+     * @return bool
+     */
+    public function uninstall()
+    {
+        if (!$this->_unregisterHooks())
+        {
+            $this->_errors[] = 'The uninstallation process of Spryng Payments for Prestashop failed. A hook could not be
+            unregistered.';
+            return false;
+        }
+
+        if (!$this->deleteConfiguration())
+        {
+            $this->_errors[] = 'Failed to delete all configuration values.';
+            return false;
+        }
+
+        $this->dropDatabaseTables();
+        return true;
     }
 
     private function dropDatabaseTables()
@@ -120,5 +141,90 @@ class SpryngPayments extends PaymentModule
         }
 
         return $this->unregisterHook($name);
+    }
+
+    protected function initializeConfiguration()
+    {
+        return
+            // General settings
+            $this->initializeConfigurationValue($this->getConfigKeyPrefix().'PLUGIN_VERSION', $this->getVersion()) &&
+            $this->initializeConfigurationValue($this->getConfigKeyPrefix().'API_KEY', '') &&
+            $this->initializeConfigurationValue($this->getConfigKeyPrefix().'SANDBOX_ENABLED', true) &&
+            $this->initializeConfigurationValue($this->getConfigKeyPrefix().'MERCHANT_REFERENCE', 'Prestashop plugun') &&
+            $this->initializeConfigurationValue($this->getConfigKeyPrefix().'PAY_BUTTON_TEXT', 'Pay with Spryng Payments') &&
+
+            // iDEAL settings
+            $this->initializeConfigurationValue($this->getConfigKeyPrefix().'IDEAL_ENABLED', false) &&
+            $this->initializeConfigurationValue($this->getConfigKeyPrefix().'IDEAL_TITLE', 'Spryng Payments - iDEAL') &&
+            $this->initializeConfigurationValue($this->getConfigKeyPrefix().'IDEAL_DESCRIPTION', 'Pay online via your own bank') &&
+            $this->initializeConfigurationValue($this->getConfigKeyPrefix().'IDEAL_ACCOUNT', '') &&
+
+            // Credit Card settings
+            $this->initializeConfigurationValue($this->getConfigKeyPrefix().'CC_ENABLED', false) &&
+            $this->initializeConfigurationValue($this->getConfigKeyPrefix().'CC_TITLE', 'Spryng Payments - CreditCard') &&
+            $this->initializeConfigurationValue($this->getConfigKeyPrefix().'CC_DESCRIPTION', 'Pay using your own CreditCard') &&
+            $this->initializeConfigurationValue($this->getConfigKeyPrefix().'CC_ACCOUNT', '') &&
+
+            // PayPal settings
+            $this->initializeConfigurationValue($this->getConfigKeyPrefix().'PAYPAL_ENABLED', false) &&
+            $this->initializeConfigurationValue($this->getConfigKeyPrefix().'PAYPAL_TITLE', 'Spryng Payments - PayPal') &&
+            $this->initializeConfigurationValue($this->getConfigKeyPrefix().'PAYPAL_DESCRIPTION', 'Pay safely using PayPal') &&
+            $this->initializeConfigurationValue($this->getConfigKeyPrefix().'PAYPAL_ACCOUNT', '');
+    }
+
+    protected function deleteConfiguration()
+    {
+        return
+            // General settings
+            $this->deleteConfigurationValue($this->getConfigKeyPrefix().'PLUGIN_VERSION') &&
+            $this->deleteConfigurationValue($this->getConfigKeyPrefix().'API_KEY') &&
+            $this->deleteConfigurationValue($this->getConfigKeyPrefix().'SANDBOX_ENABLED') &&
+            $this->deleteConfigurationValue($this->getConfigKeyPrefix().'MERCHANT_REFERENCE') &&
+            $this->deleteConfigurationValue($this->getConfigKeyPrefix().'PAY_BUTTON_TEXT') &&
+
+            // iDEAL settings
+            $this->deleteConfigurationValue($this->getConfigKeyPrefix().'IDEAL_ENABLED') &&
+            $this->deleteConfigurationValue($this->getConfigKeyPrefix().'IDEAL_TITLE') &&
+            $this->deleteConfigurationValue($this->getConfigKeyPrefix().'IDEAL_DESCRIPTION') &&
+            $this->deleteConfigurationValue($this->getConfigKeyPrefix().'IDEAL_ACCOUNT') &&
+
+            // Credit Card settings
+            $this->deleteConfigurationValue($this->getConfigKeyPrefix().'CC_ENABLED') &&
+            $this->deleteConfigurationValue($this->getConfigKeyPrefix().'CC_TITLE') &&
+            $this->deleteConfigurationValue($this->getConfigKeyPrefix().'CC_DESCRIPTION') &&
+            $this->deleteConfigurationValue($this->getConfigKeyPrefix().'CC_ACCOUNT') &&
+
+            // PayPal settings
+            $this->deleteConfigurationValue($this->getConfigKeyPrefix().'PAYPAL_ENABLED') &&
+            $this->deleteConfigurationValue($this->getConfigKeyPrefix().'PAYPAL_TITLE') &&
+            $this->deleteConfigurationValue($this->getConfigKeyPrefix().'PAYPAL_DESCRIPTION') &&
+            $this->deleteConfigurationValue($this->getConfigKeyPrefix().'PAYPAL_ACCOUNT');
+    }
+
+    protected function initializeConfigurationValue($key, $value)
+    {
+        return Configuration::updateValue($key, (Configuration::get($key) !== false) ? Configuration::get($key) :
+            $value);
+    }
+
+    protected function deleteConfigurationValue($key)
+    {
+        return Configuration::deleteByName($field);
+    }
+
+    /**
+     * @return string
+     */
+    public function getVersion()
+    {
+        return self::VERSION;
+    }
+
+    /**
+     * @return string
+     */
+    public function getConfigKeyPrefix()
+    {
+        return self::CONFIG_KEY_PREFIX;
     }
 }
